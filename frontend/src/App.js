72d0aa6d5ff6d5f97f2d758191e0cc6e2a55a803
Frede <freddyzorio12@gmail.com>
2025-05-10 16:50:22 -0600

    little commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,38 @@
 import "./App.css";
 import { Navbar } from "./Components/Navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Shop } from "./Pages/Shop";
+import { ShopCategory } from "./Pages/ShopCategory";
+import { Product } from "./Pages/Product";
+import { Cart } from "./Pages/Cart";
+import { LoginSignup } from "./Pages/LoginSignup";
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route
+            path="/disposables"
+            element={<ShopCategory category="disposable" />}
+          />
+          <Route
+            path="/eliquids"
+            element={<ShopCategory category="eliquid" />}
+          />
+          <Route
+            path="/vapeskits"
+            element={<ShopCategory category="vapeskit" />}
+          />
+          <Route path="/product" element={<Product />}>
+            <Route path=":productId" element={<Product />} />
+          </Route>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
