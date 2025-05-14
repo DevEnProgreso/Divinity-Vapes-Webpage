edf4c264cb6d387e25e07986bc660746bd6fdbba
Frede <freddyzorio12@gmail.com>
2025-05-14 15:00:18 -0600

    fourth commit

--- frontend/src/Components/Assets/data.js
+++ frontend/src/Components/Assets/data.js
@@ -1,33 +1,33 @@
 import caliPineApple from "./caliPineAppleSplash.png";
 import iplayBerryWatermelon from "./iplayBerryWatermelon.png";
-import mikeTysonPineAppleMango from "./mikeTysonPineAppleMango.png";
+import bananaNutTyson from "./BananaNutTyson.png";
 import nastyTangyAppleSplash from "./nastyTangyAppleSplash.png";
 
 let data_product = [
   {
     id: 1,
-    name: "Cali PineApple Splash",
+    name: "Cali PineApple Splash 8,500 Hits",
     image: caliPineApple,
     new_price: "$420",
     old_price: "$500",
   },
   {
     id: 2,
-    name: "Iplay Berry Watermelon",
+    name: "Iplay Berry Watermelon 2,500 Hits",
     image: iplayBerryWatermelon,
     new_price: "$250",
     old_price: "$350",
   },
   {
     id: 3,
-    name: "Mike Tyson PineApple Mango",
-    image: mikeTysonPineAppleMango,
+    name: "Mike Tyson Banana-Nut 7,500 Hits",
+    image: bananaNutTyson,
     new_price: "$450",
     old_price: "$500",
   },
   {
     id: 4,
-    name: "Nasty Tangy Apple Splash",
+    name: "Nasty Tangy Apple Splash 8,500 Hits",
     image: nastyTangyAppleSplash,
     new_price: "$500",
     old_price: "$600",
