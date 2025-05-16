7a90f13c42a1d65fba0fb4cb2c8d122c0350d93c
Frede <freddyzorio12@gmail.com>
2025-05-15 23:51:31 -0600

    sixth commit

--- frontend/src/Components/NewsLetter/NewsLetter.jsx
+++ frontend/src/Components/NewsLetter/NewsLetter.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import "./NewsLetter.css";
 
 export const NewsLetter = () => {
-  return <div className="NewsLetter"></div>;
+  return (
+    <div className="NewsLetter">
+      <h1>Get Exclusive Offers On Your Email</h1>
+      <p>Suscribe to our newletter and stay updated</p>
+      <div>
+        <input type="email" placeholder="Your Email id" />
+        <button>Suscribe</button>
+      </div>
+    </div>
+  );
 };
 
 export default NewsLetter;
