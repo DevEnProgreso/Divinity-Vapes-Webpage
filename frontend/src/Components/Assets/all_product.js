9f0050da1b04456dd519f5b335f62cfce9f1561a
Frede <freddyzorio12@gmail.com>
2025-05-21 00:31:14 -0600

    eleventh commit

--- frontend/src/Components/Assets/all_product.js
+++ frontend/src/Components/Assets/all_product.js
@@ -14,6 +14,14 @@ import chrisBrown from "./chrisBrownDesechable.png";
 import cali from "./caliBlackSeriesDesechable.png";
 import nexaUltra from "./nexaUltra.png";
 import geekBar from "./geekBarPulseX.png";
+import nastySalt from "./nastySalt.png";
+import caliJuice from "./caliJuiceSalt.png";
+import jamMonster from "./jamMonsterSalt.png";
+import juiceHead from "./juiceHeadSaltFreeze.png";
+import voopooArgus from "./voopooArgusG2PodKit.png";
+import smokNovo from "./smokNovo.png";
+import vaporessoLux from "./vaporessoLuxe.png";
+import vaporessoEcoNano from "./vaporessoEcoNano.png";
 
 let all_product = [
   {
@@ -143,6 +151,70 @@ let all_product = [
     new_price: "$600",
     old_price: "$800",
     category: "disposable",
+  },
+  {
+    id: 17,
+    name: "Nasty Salt Bad Blood 30ml",
+    image: nastySalt,
+    new_price: "$280",
+    old_price: "$350",
+    category: "eliquid",
+  },
+  {
+    id: 18,
+    name: "Cali Juice Salt Frozen Strawberry 30ml",
+    image: caliJuice,
+    new_price: "$350",
+    old_price: "$500",
+    category: "eliquid",
+  },
+  {
+    id: 19,
+    name: "JAM Monster Apple Salt 30ml",
+    image: jamMonster,
+    new_price: "$320",
+    old_price: "$500",
+    category: "eliquid",
+  },
+  {
+    id: 20,
+    name: "Juice Head Raspberry L Salt 30ml",
+    image: juiceHead,
+    new_price: "$350",
+    old_price: "$500",
+    category: "eliquid",
+  },
+  {
+    id: 21,
+    name: "Voopoo Argus G2 Pod Kit",
+    image: voopooArgus,
+    new_price: "$850",
+    old_price: "$1,200",
+    category: "vapeskit",
+  },
+  {
+    id: 22,
+    name: "Smok Novo Eco Kit",
+    image: smokNovo,
+    new_price: "$550",
+    old_price: "$1,000",
+    category: "vapeskit",
+  },
+  {
+    id: 23,
+    name: "Vaporesso Luxe XR Pod Kit",
+    image: vaporessoLux,
+    new_price: "$830",
+    old_price: "$1,000",
+    category: "vapeskit",
+  },
+  {
+    id: 24,
+    name: "Vaporesso Eco Nano Pro",
+    image: vaporessoEcoNano,
+    new_price: "$800",
+    old_price: "$1,000",
+    category: "vapeskit",
   },
 ];
 
